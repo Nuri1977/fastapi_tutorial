2f108c35fd3bbb525f709d93b5585d501e10ddbc
Nuri1977 <nurilacka@gmail.com>
2022-08-22 22:49:39 +0200

    Path and query parameters

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Union
 from fastapi import FastAPI
 from enum import Enum
 
@@ -30,3 +31,23 @@ async def get_model(model_name: ModelName):
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+# Query Parameters
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/query_items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/mix_items/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+# Request Body
